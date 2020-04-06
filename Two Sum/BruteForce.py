67caa5fa70ca60b0dd0d98b9295fa63106f4064c
Vanditg <gajjarvandit@gmail.com>
2020-04-07 06:38:09 +0930

    Adding Solution for Problem - 1 - Brute Force

---
+++ Two Sum/BruteForce.py
@@ -0,0 +1,33 @@
+##==================================
+## Leetcode
+## Student: Vandit Jyotindra Gajjar
+## Year: 2020
+## Problem: 1
+## Problem Name: Two Sum
+##===================================
+#
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+#Example:
+#
+#Given nums = [2, 7, 11, 15], target = 9,
+#
+#Because nums[0] + nums[1] = 2 + 7 = 9,
+#
+#return [0, 1].
+
+class Solution:
+    
+    def twoSum(self, array, target):
+        indices = None
+        
+        for i in range(len(array)):
+            for j in range(len(array)):
+                if  (i != j) and (array[i] + array[j]) == target:
+                    indices = [j, i]
+                else:
+                    continue
+    
+        return indices
