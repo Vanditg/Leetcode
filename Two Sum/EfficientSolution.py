bc36e79c06a489e8e5d41e59654460c267b93364
Vanditg <gajjarvandit@gmail.com>
2020-04-07 07:20:59 +0930

    Modification Comment

--- Two Sum/EfficientSolution.py
+++ Two Sum/EfficientSolution.py
@@ -17,3 +17,31 @@
 #Because nums[0] + nums[1] = 2 + 7 = 9,
 #
 #return [0, 1].
+
+class Solution:
+	def twoSum(self, array, target):
+		complementDictionary = dict() #Initializing Dictionary. Before first iteration it is empty {}
+		
+		for i in range(len(array)):   #Loop through array
+			arr = array[i]            #For every element of array
+			complement = target - arr #Find complement i.e i = 0, arr = array[0] = 2. So complement = 9 - 2 = 7
+			
+			if arr in complementDictionary:    #Condition check if array value is in dictionary
+				return [complementDictionary[arr], i]   #We'll return indices >> [complementDictionary[arr] = empty, i = 0]
+			else:
+				complementDictionary[complement] = i    #Otherwise we'll add that in our dictionary. So it will be {7 : 0}
+
+# First Iteration:
+# complementDictionary is empty. 
+# i = 0 
+# arr = array[0] = 2
+# complement = 9 - 2 = 7
+# if 2 is in dictionary (Which is not), we'll return [dictionary value for array, i]
+# else we'll add complement is our dictionary which will now {7 : 0}
+
+# Second Iteration:
+# Dictionary is {7 : 0}
+# i = 1
+# arr = array[1] = 7
+# if 7 is in dictionary (Which is), we'll return [dictionary[7] = 0, i = 1]
+#This indices will be [0, 1]
