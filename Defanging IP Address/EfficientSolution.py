4546cb7a6cfcd819a96c30212b08c2f49ca50aa3
Vanditg <gajjarvandit@gmail.com>
2020-04-07 15:40:48 +0930

    Modification Comment

--- Defanging IP Address/EfficientSolution.py
+++ Defanging IP Address/EfficientSolution.py
@@ -21,3 +21,24 @@
 #Constraints:
 #
 #The given address is a valid IPv4 address.
+class Solution:
+
+    def defangIPaddr(self, address):
+	
+        string = [] #[]
+		
+        for character in address:
+			
+            if character == '.':   #1.1.1.1 if . is in address
+				
+                string.append("[.]")    #we'll append it by ["[.]"]
+			
+            else:
+				
+                string.append(character) #Or we'll append it with characters
+		
+        return "".join(string)  #Finally we'll join the string which will give "1[.]1[.]1[.]1"
+.
+#Initialize empty string
+#For every character in our IP Address, we will check with the condition that if "." is in the address, we'll simply replace with our given value which is "[.]", 
+#otherwise, we'll append remaining address. Lastly, we'll simply join all the character in string. 
