86609f626da6d773fcc0529646ab73bde8018905
Vanditg <gajjarvandit@gmail.com>
2020-04-07 15:21:20 +0930

    Adding Solution for Problem - 1108 - Efficient Solution

---
+++ Defanging IP Address/EfficientSolution.py
@@ -0,0 +1,23 @@
+##==================================
+## Leetcode
+## Student: Vandit Jyotindra Gajjar
+## Year: 2020
+## Problem: 1108
+## Problem Name: Defanging an IP Address
+##===================================
+#Given a valid (IPv4) IP address, return a defanged version of that IP address.
+#
+#A defanged IP address replaces every period "." with "[.]".
+#
+#Example 1:
+#
+#Input: address = "1.1.1.1"
+#Output: "1[.]1[.]1[.]1"
+#Example 2:
+#
+#Input: address = "255.100.50.0"
+#Output: "255[.]100[.]50[.]0"
+#
+#Constraints:
+#
+#The given address is a valid IPv4 address.
