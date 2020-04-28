61431ba778eeeaf970d4e04ad149788683441d0d
Vanditg <gajjarvandit@gmail.com>
2020-04-28 15:37:51 +0930

    Adding Simple Solution for Problem - 217 - Contains Duplicate

---
+++ Contains_Duplicate/SimpleSolution.py
@@ -0,0 +1,28 @@
+##==================================
+## Leetcode
+## Student: Vandit Jyotindra Gajjar
+## Year: 2020
+## Problem: 217
+## Problem Name: Contains Duplicate  
+##===================================
+#
+#Given an array of integers, find if the array contains any duplicates.
+#
+#Your function should return true if any value appears at least twice in the array, 
+#and it should return false if every element is distinct.
+#
+#Example 1:
+#
+#Input: [1,2,3,1]
+#Output: true
+#Example 2:
+#
+#Input: [1,2,3,4]
+#Output: false
+#Example 3:
+#
+#Input: [1,1,1,3,3,4,3,2,4,2]
+#Output: true
+class Solution:
+    def containsDuplicate(self, nums):
+        return len(nums) > len(set(nums))	#We return True if the number appears twice times in set 
