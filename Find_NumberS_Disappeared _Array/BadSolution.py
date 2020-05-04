e84b5ac7c929ea83c0f478c64728addfe61632ca
Vanditg <gajjarvandit@gmail.com>
2020-05-04 20:47:59 +0930

    Adding Bad Solution - Problem - 448 - Find Numbers Disappeared Array

---
+++ Find_NumberS_Disappeared _Array/BadSolution.py
@@ -0,0 +1,28 @@
+##==================================
+## Leetcode
+## Student: Vandit Jyotindra Gajjar
+## Year: 2020
+## Problem: 448
+## Problem Name: Final All Numbers Disappeared in an Array           
+##===================================
+#
+#Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), 
+#some elements appear twice and others appear once.
+#
+#Find all the elements of [1, n] inclusive that do not appear in this array.
+#
+#Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+#
+#Example:
+#
+#Input:
+#[4,3,2,7,8,2,3,1]
+#
+#Output:
+#[5,6]
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        tmp = []	#Initialize tmp list 
+        for i in range(1, len(nums) + 1, 1):	#Loop through nums list
+            tmp.append(i)	#Append the i in tmp list 
+        return list(set(tmp) - set(nums))	#Return the remaining numbers
