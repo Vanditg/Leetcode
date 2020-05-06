a1bcf600f6a658e1b2ca4ada27dc7b9eebc5c593
Vanditg <gajjarvandit@gmail.com>
2020-05-06 13:53:16 +0930

    Adding Simple Solution - 190 - Reverse Bits

---
+++ Reverse_Bits/SimpleSolution.py
@@ -0,0 +1,25 @@
+##==================================
+## Leetcode
+## Student: Vandit Jyotindra Gajjar
+## Year: 2020
+## Problem: 190
+## Problem Name: Reverse Bits            
+##===================================
+#
+#Reverse bits of a given 32 bits unsigned integer.
+#
+#Example 1:
+#
+#Input: 00000010100101000001111010011100
+#Output: 00111001011110000010100101000000
+#Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+#Example 2:
+#
+#Input: 11111111111111111111111111111101
+#Output: 10111111111111111111111111111111
+#Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+class Solution:
+    def reverseBits(self, n):
+        tmp = '{0:032b}'.format(n)	#Initialize tmp and find binary representation
+        tmpReverse = tmp[::-1]	#Reverse the tmp 
+        return int(tmpReverse, 2)	#Return tmpReverse base 2
